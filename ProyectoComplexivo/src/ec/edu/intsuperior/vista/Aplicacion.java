8530bade6cae060a7e06c9d425ec250bfabcf429
nicsune98 <wcuascota123@gmail.com>
2019-04-30 21:21:05 -0500

    Versión inicial del proyecto

---
+++ ProyectoComplexivo/src/ec/edu/intsuperior/vista/Aplicacion.java
@@ -0,0 +1,17 @@
+package ec.edu.intsuperior.vista;
+
+/**
+ *
+ * @author Wilson
+ */
+public class Aplicacion {
+    public static void main(String[] args) {
+        System.out.println(meta());
+    }
+    public static int meta(){
+        return (int)(Math.random()*100);
+    }
+    public void metb(){
+        System.out.println("Estudiarán");
+    }
+}
