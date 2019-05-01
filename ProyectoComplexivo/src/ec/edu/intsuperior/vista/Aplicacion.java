a065773456a46d03a412f3212e4110d1776a3353
wssolis <sys.solis@gmail.com>
2019-04-30 21:21:05 -0500

    Version inicial del proyecto

---
+++ ProyectoComplexivo/src/ec/edu/intsuperior/vista/Aplicacion.java
@@ -0,0 +1,17 @@
+package ec.edu.intsuperior.vista;
+
+
+
+public class Aplicacion {
+    public static void main(String[] args) {
+        System.out.println(metA());
+        
+    }
+    
+    public static int metA(){
+        return (int)(Math.random()*10);
+    }
+    public void metB(){
+        System.out.println("Estudiaran");
+    }
+}
