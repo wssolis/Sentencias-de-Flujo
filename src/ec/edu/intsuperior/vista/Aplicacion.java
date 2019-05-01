d346d9e60d4e7a6a5586014d659ed53d2f5300e7
DiegO <DiegO@DiegO>
2019-04-30 21:30:17 -0500

    veron inicial

---
+++ src/ec/edu/intsuperior/vista/Aplicacion.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.intsuperior.vista;
+
+/**
+ *
+ * @author DiegO
+ */
+public class Aplicacion {
+    public static void main(String[] args) {
+        System.out.println(metA());
+    }
+    public static int metA(){
+        return (int)(Math.random()*10);
+    }
+     public void metB(){     
+         System.out.println("Estudiaran");
+        }
+
+}
