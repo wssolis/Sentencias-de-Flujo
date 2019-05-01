cdb219730473e8a0cd65df3cdc11b8ed871b0f17
nicsune98 <wcuascota123@gmail.com>
2019-05-01 16:15:51 -0500

    Proyecto con menú

--- ProyectoComplexivo/src/ec/edu/intsuperior/vista/Aplicacion.java
+++ ProyectoComplexivo/src/ec/edu/intsuperior/vista/Aplicacion.java
@@ -1,24 +1,48 @@
 package ec.edu.intsuperior.vista;
 
+import ec.edu.intsuperior.controlador.Calculos;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  *
  * @author Wilson
  */
 public class Aplicacion {
     public static void main(String[] args) {
-        validarParidad();
-        
+        menu();
     }
-    public static int meta(){
-        return (int)(Math.random()*100);
-    }
-    public static void validarParidad(){
-        int x =meta();
-        if (meta()%2==0) {
-            System.out.println(x+" Es par");
-            
-        } else {
-            System.out.println(x+" Es impar");
-        }
+    public static void menu(){
+        Scanner entrada = new Scanner(System.in);
+        int opcion=0;
+        boolean salir = false;
+        while(!salir){
+            Calculos operacion = new Calculos();
+        System.out.printf("\tMenú\n1.- Número aleatorio\n2.- Par o Impar de un número aleatorio\n3.- Factorial de un número aleatorio\n4.- Salir\n\nIngresa una opcion --> ");
+            try {
+                opcion=entrada.nextInt();
+                while(opcion>=5){
+                    System.out.println("Solo ingresa una opcion del menú");
+                    opcion=entrada.nextInt();
+                }
+            switch (opcion){
+                    case 1:
+                        System.out.printf("El número aleatorio es %d\n",operacion.meta());
+                        break;
+                    case 2:
+                        operacion.validarParidad();
+                        break;
+                    case 3:
+                        operacion.factorial();
+                        break;
+                    case 4:
+                        salir=true;
+                        System.out.println("Gracias por usar el software");
+            }
+            } catch (InputMismatchException e) {
+                System.out.println("Se debe ingresar solo números");
+                entrada.next();
+            }
+        }     
     }
 }
