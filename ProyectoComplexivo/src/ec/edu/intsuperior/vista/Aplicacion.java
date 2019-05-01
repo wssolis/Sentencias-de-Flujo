4cf0adb7da20b4ec91e415d3fe64caa0c24c2f38
wssolis <sys.solis@gmail.com>
2019-04-30 21:47:36 -0500

    Version dos del proyecto

--- ProyectoComplexivo/src/ec/edu/intsuperior/vista/Aplicacion.java
+++ ProyectoComplexivo/src/ec/edu/intsuperior/vista/Aplicacion.java
@@ -4,14 +4,21 @@ package ec.edu.intsuperior.vista;
 
 public class Aplicacion {
     public static void main(String[] args) {
-        System.out.println(metA());
+        validarParidad();
         
     }
     
     public static int metA(){
         return (int)(Math.random()*10);
     }
-    public void metB(){
-        System.out.println("Estudiaran");
+    public static void validarParidad(){
+        int x=metA();
+        if (x%2==0) {
+            System.out.println("El numero"+x+"Es par");
+        } else {
+             System.out.println("El numero"+x+"Es impar");
+        }
+        
+        
     }
 }
